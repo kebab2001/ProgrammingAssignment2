d30e38a8bd7a45aded9613c83abb7a7217a6c0d1
Eino Palin <eino.palin@iki.fi>
2014-05-23 21:31:08 +0300

    - Added the descriptions - Caching works as planned

--- cachematrix.R
+++ cachematrix.R
@@ -1,21 +1,23 @@
-## Put comments here that give an overall description of what your
-## functions do
-##The first function, makeVector creates a special "vector", which is really a list containing a function to
-##set the value of the vector
-##get the value of the vector
-##set the value of the mean
-##get the value of the mean
-## makeCacheMatrix: This function creates a special "matrix" object that can cache its inverse.
+## makeCacheMatrix: This function creates a special "matrix" object that can cache its inverse (=setCache).
 
 makeCacheMatrix <- function(x = matrix()) {
+## Set cache to null if no subfunctions are called        
         m <- NULL
+## Set subfunction: set x to y and m to null
         set <- function(y) {
                 x <<- y
                 m <<- NULL
         }
+## Returns the matrix stored in x
         get <- function() x
+
+## Saves the inversed matrix to setCache
         setCache <- function(solve) m <<- solve
+
+## Returns the cache
         getCache <- function() m
+
+## List of all possible subfunctions (e.g. makeCacheMatrix$get), which can be called
         list(set = set, get = get,
              setCache = setCache,
              getCache = getCache)
@@ -27,15 +29,23 @@ makeCacheMatrix <- function(x = matrix()) {
 ## then the cachesolve should retrieve the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-        
+## Fetches the cache         
         m <- x$getCache()
+## Checks that if the cache is null. If not, returns message that we're using cached data
+        
         if(!is.null(m)) {
                 message("getting cached data")
                 return(m)
         }
+
+## Sets data to be the matrix
         data <- x$get()
+## Calculates the inverse of the matrix
         m <- solve(data, ...)
+
+## Sets the cache to the calculated inverse of matrix
         x$setCache(m)
+
+## Returns the inverse
         m
 }
